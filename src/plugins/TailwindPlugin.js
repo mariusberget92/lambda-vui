a52b1864fdb65783883ed35c2df5515f827de2c8
Marius Berget <mariusberget92@protonmail.com>
2023-07-22 14:29:28 +0200

    removed array and join, only returning one string

--- src/plugins/TailwindPlugin.js
+++ src/plugins/TailwindPlugin.js
@@ -2,21 +2,21 @@ export default {
   install: (app) => {
     app.config.globalProperties.$sizeToClass = (size) => {
       const sizes = {
-        xs: ['text-xs'],
-        sm: ['text-sm'],
-        base: ['text-base'],
-        lg: ['text-lg'],
-        xl: ['text-xl'],
-        '2xl': ['text-2xl'],
-        '3xl': ['text-3xl'],
-        '4xl': ['text-4xl'],
-        '5xl': ['text-5xl'],
-        '6xl': ['text-6xl'],
-        '7xl': ['text-7xl'],
-        '8xl': ['text-8xl'],
-        '9xl': ['text-9xl'],
+        xs: 'text-xs',
+        sm: 'text-sm',
+        base: 'text-base',
+        lg: 'text-lg',
+        xl: 'text-xl',
+        '2xl': 'text-2xl',
+        '3xl': 'text-3xl',
+        '4xl': 'text-4xl',
+        '5xl': 'text-5xl',
+        '6xl': 'text-6xl',
+        '7xl': 'text-7xl',
+        '8xl': 'text-8xl',
+        '9xl': 'text-9xl',
       }
-      return sizes[size].join(' ')
+      return sizes[size]
     }
   },
 }
